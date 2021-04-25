b6f48eea5e69efeee806ee4b560cd53f55665067
KellyButic <49516066+KellyButic@users.noreply.github.com>
2021-04-26 00:43:23 +1000

    Folder and Files Creation with Initialization

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/KellyButic/AWS-Assignment2-CC
+
+go 1.16
+
+require github.com/gorilla/mux v1.8.0 // indirect
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	//	"github.com/eoria17/AWS-Golang-Music-Sub/controllers"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+}
